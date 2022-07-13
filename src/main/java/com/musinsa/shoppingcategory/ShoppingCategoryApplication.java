f410f544fd981a246150252d46f83a2c21bd3e8c
malaheaven <yunhg95@gmail.com>
2022-07-13 17:19:10 +0900

    [ADD] Project init

---
+++ src/main/java/com/musinsa/shoppingcategory/ShoppingCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.shoppingcategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCategoryApplication.class, args);
+    }
+
+}

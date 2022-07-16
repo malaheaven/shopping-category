0e1eaf5b0d125bc160a4cd3c0f4d68cde9806ff6
malaheaven <yunhg95@gmail.com>
2022-07-16 15:46:43 +0900

    [ADD] BaseConfig 생성

---
+++ src/main/java/com/musinsa/shoppingcategory/constants/BaseConfig.java
@@ -0,0 +1,20 @@
+package com.musinsa.shoppingcategory.constants;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+public class BaseConfig {
+
+    @Configuration
+    @EnableJpaAuditing
+    public static class JpaConfig {
+
+    }
+
+    @Configuration
+    @EnableCaching
+    public static class cacheConfig {
+
+    }
+}

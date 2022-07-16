d3c9a8d3e4673fdcf318213c70651173a987b715
malaheaven <yunhg95@gmail.com>
2022-07-17 01:02:47 +0900

    [ADD] sh build 추가

--- local-start.sh
+++ local-start.sh
@@ -1,4 +1,5 @@
 # local-start.sh
-./gradlew bootJar                   # spring boot bootJar
+./gradlew build                     # spring boot build
+./gradlew bootJar                   # spring boot bootJar -> spring rest docs index.html 생성을 위해
 cd build/libs/                      # move build/libs/
 java -Dspring.profiles.active=local -jar shopping-category-0.0.1-SNAPSHOT.jar # start local spring boot
